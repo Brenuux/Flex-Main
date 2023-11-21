9cce1f8d11121d8b5ad91c087027fb859a46c155
Brenuux <brenolfm@unipam.edu.br>
2023-11-21 14:23:17 -0300

    Area do perfil cliente concluida

--- components/Pages/Perfil/index.js
+++ components/Pages/Perfil/index.js
@@ -9,9 +9,12 @@ import Heart from '../../img/Heart.svg';
 import Star from '../../img/Star.svg';
 import Arrow from '../../img/ArrowLeft.svg';
 import BatePapo from '../../img/BatePapo.svg';
+import Logout from '../../img/Logout.svg';
+import { useNavigation } from "@react-navigation/native";
 
 export default function Perfil() {
   const [userName, setUserName] = useState('');
+  const navigation = useNavigation();
 
   // UseEffect para carregar o nome do usuário a partir do Firebase
   useEffect(() => {
@@ -37,53 +40,67 @@ export default function Perfil() {
   return (
     <SafeAreaView>
     <ScrollView persistentScrollbar={true} showsVerticalScrollIndicator={false}>
-      <View>
-        <TouchableOpacity>
-          <UserP style={{ alignSelf: 'center', top: 45 }}/>
+      <View style={{flexDirection: 'row', marginTop: 10}}>
+        <Text style={{fontSize: 30, left: 170, fontFamily: 'Segoe UI Bold'}}>FLEX</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Entrar')}>
+          <Logout style={{left: 260}}/>
         </TouchableOpacity>
+      </View>
+      <View>
+          <UserP style={{ alignSelf: 'center', top: 45 }}/>
         <Text style={{ fontSize: 20, fontFamily: 'Segoe UI Bold', top: 66, left: 177 }}>{userName}</Text>
       </View>
-      <View style={{ flexDirection: 'row', top: 30}}>
         <TouchableOpacity style={styles.btns}>
+          <View style={{ top: 40}}>
           <BatePapo style={styles.icons} />
           <Text style={styles.textBtn}>Conversas</Text>
           <Text style={[styles.textBtn, { marginTop: 10, fontFamily: 'Segoe UI', fontSize: 16 }]}>Histórico de conversas</Text>
+          </View>
         </TouchableOpacity>
-      </View>
+      
       <TouchableOpacity style={styles.btns}>
+        <View style={{ top: 10}}>
         <Ring style={styles.icons} />
         <Text style={styles.textBtn}>Notificações</Text>
         <Text style={[styles.textBtn, { marginTop: 10, fontFamily: 'Segoe UI', fontSize: 16 }]}>Central de Notificações</Text>
+        </View>
       </TouchableOpacity>
-
+      
+      <View style={{flexDirection:'row', top: -10}}>
       <TouchableOpacity style={styles.btns}>
         <Wallet style={styles.icons} />
         <Text style={styles.textBtn}>Pagamentos</Text>
         <Text style={[styles.textBtn, { marginTop: 10, fontFamily: 'Segoe UI', fontSize: 16 }]}>Saldos e Cartões</Text>
       </TouchableOpacity>
-
+      </View>
       <TouchableOpacity style={styles.btns}>
+        <View style={{top: -30}}>
         <Ticket style={styles.icons} />
         <Text style={styles.textBtn}>Cupons</Text>
         <Text style={[styles.textBtn, { marginTop: 10, fontFamily: 'Segoe UI', fontSize: 16 }]}>Cupons de Descontos</Text>
+        </View>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.btns}>
+        <View style={{top: -55}}>
         <Star style={styles.icons} />
         <Text style={styles.textBtn}>Fidelidade</Text>
         <Text style={[styles.textBtn, { marginTop: 10, fontFamily: 'Segoe UI', fontSize: 16 }]}>Minhas Fidelidades</Text>
+        </View>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.btns}>
+        <View style={{top: -85}}>
         <Heart style={styles.icons} />
         <Text style={styles.textBtn}>Favoritos</Text>
         <Text style={[styles.textBtn, { marginTop: 10, fontFamily: 'Segoe UI', fontSize: 16 }]}>Locais Favoritos</Text>
+        </View>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.btns}>
         <Text></Text>
       </TouchableOpacity>
-      
+
       <TouchableOpacity style={styles.btns}>
       <Text></Text>
       <Text></Text>
